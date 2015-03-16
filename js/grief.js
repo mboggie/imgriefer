1437e3786c715e90dcc9d2e745a0f47322b4a7dd
Matt Boggie <mboggie@me.com>
2015-03-15 21:25:13 -0400

    updated styling and controls

--- js/grief.js
+++ js/grief.js
@@ -6,7 +6,12 @@ var startButton = document.getElementById('startButton');
     startButton.addEventListener('click', start, false);
 var stopButton = document.getElementById('stopButton');
     stopButton.addEventListener('click', stop, false);
+var saveButton = document.getElementById('saveButton');
+    saveButton.addEventListener('click', save, false);
 var keepGoing = 0;
+var mutations = 0;
+var blocksize = 10;
+var speed = 100;
 
 function handleImage(e){
     var reader = new FileReader();
@@ -14,13 +19,24 @@ function handleImage(e){
         var img = new Image();
         img.onload = function(){
             /* TODO: Constrain image to the window width */
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img,0,0);
+            var maxHeight = window.innerHeight - 100
+            canvas.width = document.getElementById('image-wrapper').offsetWidth;
+            canvas.height = maxHeight*2;
+
+            //ctx.drawImage(img,0,0);
+            if (img.width > img.height) {
+                ctx.drawImage(img, 0, 0, window.innerWidth, window.innerWidth * img.height / img.width)
+            }
+            else {
+                ctx.drawImage(img, 0, 0, maxHeight * img.width / img.height, maxHeight)
+            }
+            
         }
         img.src = event.target.result;
     }
-    reader.readAsDataURL(e.target.files[0]);     
+    reader.readAsDataURL(e.target.files[0]); 
+
+    document.getElementById('controls').style.display = "block";    
 }
 
 function start(){
@@ -32,36 +48,43 @@ function stop(){
     keepGoing = 0;
 }
 
+function save() {
+    console.log("saving")
+    document.location.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+}
 
 function grief(){
+    console.log(blocksize)
 
     /*console.log(Math.random());*/
     var ax = Math.floor(Math.random() * canvas.width);
     var ay = Math.floor(Math.random() * canvas.height);
-    console.log("a is " + ax + ", " + ay +".");
 
-    var pixelA = ctx.getImageData(ax, ay, 10, 10);
+    var pixelA = ctx.getImageData(ax, ay, blocksize, blocksize);
 
     /* random < 0.5: horizontal; random > 0.5: vertical */
-    var direction = (Math.random < 0.5) ? "x" : "y";
-    var offset = (Math.random < 0.5) ? -1 : 1;
+    var direction = (Math.random() < 0.5) ? "x" : "y";
+    var offset = (Math.random() < 0.5) ? -1 : 1;
     var pixelB, bx, by;
 
     if (direction == "x") {
-        bx = ax + (offset*10);
+        bx = ax + (offset*blocksize);
         by = ay;
     }
     else
     {
         bx = ax;
-        by = ay + (offset*10);
+        by = ay + (offset*blocksize);
     }
-    var pixelB = ctx.getImageData(bx, by, 10, 10);
-    console.log("b is " + bx + ", " + by + ".");
+    var pixelB = ctx.getImageData(bx, by, blocksize, blocksize);
+
     ctx.putImageData(pixelA, bx, by);
     ctx.putImageData(pixelB, ax, ay);
+    mutations++;
+    mutcount = document.getElementById("mutcount");
+    mutcount.innerText = mutations;
 
     if (keepGoing > 0) {
-        setTimeout(grief, 100);
+        setTimeout(grief, speed);
     }
 }
